8d8d9aa1a2c8bef3a11f4a03aa2905056aee6e3f
Ashton Dudley <adudley@dal.ca>
2026-02-18 22:27:21 -0400

    Fix TIM15 initialization parameters

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -180,15 +180,9 @@ void MX_TIM15_Init(void)
 
   /* USER CODE END TIM15_Init 1 */
   htim15.Instance = TIM15;
-<<<<<<< working-fsk
-  htim15.Init.Prescaler = 64000-1;
-  htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim15.Init.Period = 10-1;
-=======
   htim15.Init.Prescaler = 6400-1;
   htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim15.Init.Period = 91-1;
->>>>>>> 18-feat-implement-fsk
   htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim15.Init.RepetitionCounter = 0;
   htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
