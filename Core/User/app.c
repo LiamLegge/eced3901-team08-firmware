ff7e5b388adfc09fa3e8e00a7733e12b5fcde595
Ashton Dudley <adudley@dal.ca>
2026-02-18 22:22:37 -0400

    Moved externs

--- Core/User/app.c
+++ Core/User/app.c
@@ -7,11 +7,6 @@
 #include "FSK.h"
 #include <stdint.h>
 
-extern TIM_HandleTypeDef htim1;
-extern TIM_HandleTypeDef htim2;
-extern TIM_HandleTypeDef htim15;
-extern DMA_HandleTypeDef hdma_tim1_ch1;
-
 #define FRAMEDELAY 10
 
 void app(void) {
