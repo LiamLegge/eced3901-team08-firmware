6a493cd92b13e1b80717278337438e14f31ab10d
Ashton Dudley <adudley@dal.ca>
2026-02-18 22:32:12 -0400

    Modify FSK functions to accept pwm_data
    Updated init_fsk and start_fsk functions to include pwm_data parameter.

--- Core/User/FSK.h
+++ Core/User/FSK.h
@@ -9,14 +9,8 @@
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim15;
 
-<<<<<<< working-fsk
 void init_fsk(uint8_t* msg);
 
 void start_fsk(void);
-=======
-void init_fsk(uint8_t* msg, uint32_t *pwm_data);
-
-void start_fsk(uint32_t *pwm_data);
->>>>>>> 18-feat-implement-fsk
 
 void stop_fsk(void);
