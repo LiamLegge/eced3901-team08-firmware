ed58826617f8e0329a2433f17204424eeffff48b
Ashton Dudley <adudley@dal.ca>
2026-02-21 00:17:29 -0400

    Add USART support and expand command enum

--- Core/User/commands.h
+++ Core/User/commands.h
@@ -1,12 +1,16 @@
 #pragma once
 
-typedef enum
-{
-    CMD_NONE = 0,
-    CMD_RUN,
-    CMD_HALT,
-    CMD_COUNT        // always keep last
-} Command_t;
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+    CMD_NONE            = 0x00,
+    CMD_RUN             = 0x01,
+    CMD_HALT            = 0x02,
+    CMD_EMAG_ENABLE     = 0xf1,
+    CMD_EMAG_DISABLE    = 0xf2,
+    CMD_EMAG_TOGGLE     = 0xf3
+} t_command;
 
 
 void init_commands(void);
