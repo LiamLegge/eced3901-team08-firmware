74cbb62d679d79e01a71be420454152545263abd
Ashton Dudley <adudley@dal.ca>
2026-03-03 13:05:17 -0400

    65 Added cargo protype (#66)
    * deleted uart handles
    
    * Add USART support and expand command enum
    
    * Added cargo prototype
    
    Code is being tested on an STM32G0316
    
    * Pass cmd to cargo_main and include cargo.h
    
    * 68 moved fsk pin to PA5 (#69)
    
    * Updated IOC file, moved FSK to PA5
    
    * added  FSK_TIMER_CHANNEL macro
    
    * 68 add adc (#70)
    
    * Added docs
    
    * Generated ADC data with DMA
    
    * Removed ADC DMA
    
    * Generated GPIO setup
    
    * Added ADC
    
    * Working build
    
    todo: test on hardware
    
    * Use external hadc1 and pointer ADC_HANDLE
    
    * Working on setting up ADC
    
    * Disabled ADC IRQ
    
    * Boots with ADC
    
    Work in progress
    
    * Slowed heartbeat
    
    * Log raw ADC EMAG voltage and use direct ADC value
    
    Read and log the EMAG ADC value in cargo_main by calling get_adc_value() and printing it when VERBOSE is enabled. In HAL_ADC_ConvCpltCallback, stop mapping the ADC sample to a percentage and store the raw adc_buffer[0] value in adc_value instead. This preserves the raw ADC reading for further processing and makes the runtime EMAG voltage observable in logs.
    
    * Add ADC window check and emag logging (#79)
    
    * Improved serial output
    
    -----------------------------
    [ APP ] Time: 38385 ms
    [ ADC ] Current value: 1011
    [CARGO] ENTERED window (v=823, win=[652..960])
    -----------------------------
    [ APP ] Time: 38796 ms
    [ ADC ] Current value: 822
    [CARGO] LEFT window (v=1008, win=[652..960])
    [TOPIC] heartbeat time: 39215
    [CARGO] ENTERED window (v=954, win=[652..960])
    -----------------------------
    [ APP ] Time: 39215 ms
    [ ADC ] Current value: 900
    -----------------------------
    [ APP ] Time: 39632 ms
    [ ADC ] Current value: 981
    [CARGO] LEFT window (v=1009, win=[652..960])
    [CARGO] ENTERED window (v=837, win=[652..960])
    -----------------------------
    [ APP ] Time: 40047 ms
    [ ADC ] Current value: 833
    [TOPIC] heartbeat time: 40260
    [CARGO] LEFT window (v=1006, win=[652..960])
    [CARGO] ENTERED window (v=895, win=[652..960])
    -----------------------------
    [ APP ] Time: 40469 ms
    [ ADC ] Current value: 950
    [CARGO] LEFT window (v=998, win=[652..960])
    -----------------------------
    [ APP ] Time: 40884 ms
    [ ADC ] Current value: 953
    [CARGO] ENTERED window (v=822, win=[652..960])
    [TOPIC] heartbeat time: 41303
    [CARGO] LEFT window (v=994, win=[652..960])
    -----------------------------

--- Core/User/app.c
+++ Core/User/app.c
@@ -7,11 +7,12 @@
 // #include "led.h"
 #include "fsk.h"
 // #include "sensor.h"
-// #include "cargo.h"
+#include "cargo.h"
 #include "stm32_ros_topic.h"
 #include "stm32g0xx_hal_gpio.h"
 #include "logging.h"
 #include "commands.h"
+#include <stdint.h>
 
 // Handles
 extern UART_HandleTypeDef huart2;
@@ -24,9 +25,6 @@ __weak void sensor_main(void) {}
 
 __weak void init_sr04(void) {}
 __weak void sr04_main(void) {}
-
-__weak void init_cargo(void)  {}
-__weak void cargo_main(void)  {}
 
 
 // Subsystem startup functions
@@ -41,19 +39,32 @@ void app_init(void)
     init_cargo();
 }
 
+void print_status_update(uint32_t time_ms) {
+    const uint32_t log_interval_ms = 250;
+    static uint32_t last_log_time = 0;
+
+    if ((time_ms - last_log_time) >= log_interval_ms) {
+        last_log_time = time_ms;
+
+        // Print log message with a timestamp 
+        print_log("-----------------------------");
+        print_log("[ APP ] Time: %lu ms", time_ms);
+        print_adc_value();
+    }
+}
 
 // Application main loop    
 void app(void)
 {
     app_init();
-    print_log("[ LOG ] === SYSTEM START ===");
+    print_log("[ APP ] === SYSTEM START ===");
 
     char cmd = '0';
-
+    uint32_t time_ms = 0;
     for (;;)
     {
         profile_begin();
-
+        time_ms = HAL_GetTick();
         cmd = command_main();
 
         if (cmd == 0x01) {
@@ -64,9 +75,10 @@ void app(void)
         ros_topic_main();
         led_main();
         sr04_main();
-        cargo_main();
+        cargo_main(cmd);
         
-        profile_end();
         HAL_Delay(1);
+        print_status_update(time_ms);
+        profile_end();
     }
 }
--- Core/User/stm32_ros_topic.h
+++ Core/User/stm32_ros_topic.h
@@ -3,11 +3,8 @@
 #include "stdbool.h"
 #include "stdint.h"
 
-
-
-
+#define HEARTBEAT_INTERVAL_MS 1000
 
 void ros_topic_main(void);
 
 /* Commands for firmware functions */
-
